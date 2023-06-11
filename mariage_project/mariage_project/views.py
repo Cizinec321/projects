7cc5f1ad184e67890723176176218693fd237aaf
PCDORMITOR\matei <velich.eduard@gmail.com>
2023-06-11 19:18:20 +0300

    Moved url parameters for page sqwitching to actually using javascript (switching from hidden to visible in css). Changed some buttons on main page moved login form code into main home fuction ----all of these shoudl imrpove performance (and are a generally better way of doing things)

--- mariage_project/mariage_project/views.py
+++ mariage_project/mariage_project/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 import datetime
 from django.http import HttpResponse
-from .forms import input_form, m_input_form, seating_generator, invitees_form,invitees_disp
+from .forms import input_form, m_input_form, seating_generator, invitees_form,invitees_disp,loginform
 from .models import in_form, tables, models, invitees,invitees_x_table
 from . import identity_management as im
 from . import show_text as st
@@ -16,6 +16,7 @@ global_text=''
 menu_trig=False
 #OK HERE
 def home(request):
+    
     agent = request.META["HTTP_USER_AGENT"]
     global global_text
     global menu_trig
@@ -26,9 +27,10 @@ def home(request):
                 return render(request,'m_landing_log.html',{'display_trigger':'<div id="menubtn" class="dropdown-content" style="display:none">','click_check_div3':m.mobile_generator('aboutus')})
     else:
         if im.get_uname(request)=='Not loged in.':
-                return render(request,'landing.html')
+                #move form wrapers to template!!!!!
+                return render(request,'landing.html',{'click_check_div3':gen_dat.gen_text(),'click_form_login':loginform(),'form_wrapper_start':'<div class="dropbtn2" id="login"><form method="post"><table style="width:100%">','form_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2">Submit</button></th><td></td></tr></table></form></div>',})
         else:
-                return render(request,'landing_log.html')
+                return render(request,'landing_log.html',{'click_check_div3':gen_dat.gen_text(),'click_form_login':loginform(),'form_wrapper_start':'<div class="dropbtn2" id="login"><form method="post"><table style="width:100%">','form_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2">Submit</button></th><td></td></tr></table></form></div>',})
 #OK HERE
 def show_txt(request):
     global global_text
