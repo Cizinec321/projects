a240836ab4c1059669ee34620efbc79b334c8dff
PCDORMITOR\matei <velich.eduard@gmail.com>
2023-08-04 16:18:17 +0300

    1) Fixed seat loading func 2) Added sqlite3 executable for easy data access

--- mariage_project/mariage_project/views.py
+++ mariage_project/mariage_project/views.py
@@ -7,8 +7,9 @@ from . import identity_management as im
 from . import show_text as st
 from . import gen_dat
 from . import mobile as m
-
-
+from . import seating_algo
+from .models import in_form, tables, models, invitees,invitees_x_table
+from django.db.models import Max
 
 #OK HERE
 def home(request):
@@ -33,9 +34,19 @@ def home(request):
                 login(request,user)
         if request.POST.get("logout_request"):
               logout(request)
+        if request.POST.get("seating_request"):
+              template=request.POST['template']
+              if template!='':
+                seating_table=seating_algo.seat_load(template)
+                return render(request,'landing_log.html',{'click_check_div3':gen_dat.gen_text(),'click_form_login':login_form,'form_wrapper_start':'<div class="dropbtn2" id="login"><form method="post"  name="login_request"><table style="width:100%">','form_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2" name="login_request" value="1">Submit</button></th><td></td></tr></table></form></div>','click_form_register':register_form,'click_form_seating':seating_table,'registerform_wrapper_start':'<div class="dropbtn2" id="register"><form method="post"  name="login_request"><table style="width:100%">','registerform_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2" name="login_request" value="1" >Submit</button></th><td></td></tr></table></form></div>','seatingform_wrapper_start':'<div class="dropbtn2_alt_show" id="seating"><form method="post"><button type="submit" class="pos_button_unload" value="1">Unload</button></form>','seatingform_wrapper_end':'</div>'})
+   
+                
+              else:
+                print('No tempalte')
         if request.user.is_authenticated:
                 return render(request,'landing_log.html',{'click_check_div3':gen_dat.gen_text(),'click_form_login':login_form,'form_wrapper_start':'<div class="dropbtn2" id="login"><form method="post"  name="login_request"><table style="width:100%">','form_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2" name="login_request" value="1">Submit</button></th><td></td></tr></table></form></div>','click_form_register':register_form,'click_form_seating':seating_form,'registerform_wrapper_start':'<div class="dropbtn2" id="register"><form method="post"  name="login_request"><table style="width:100%">','registerform_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2" name="login_request" value="1" >Submit</button></th><td></td></tr></table></form></div>','seatingform_wrapper_start':'<div class="dropbtn2" id="seating"><form method="post"  name="seating_request"><table style="width:100%">','seatingform_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2" name="seating_request" value="1" >Submit</button></th><td></td></tr></table></form></div>'})
 
         else:
                 return render(request,'landing.html',{'click_check_div3':gen_dat.gen_text(),'click_form_login':login_form,'form_wrapper_start':'<div class="dropbtn2" id="login"><form method="post"  name="login_request"><table style="width:100%">','form_wrapper_end':'<tr><th></th><th><button type="submit" class="pos_button2" name="login_request" value="1" >Submit</button></th><td></td></tr></table></form></div>'})
 
+ 
